fb48151d6c18eb939f43f3c047516fd491268655
TiiaJohanna <n3titi00@students.oamk.fi>
2024-10-25 22:16:08 +0300

    Added products

---
+++ src/OrderForm.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import OrderInfo from "./OrderInfo";
+
+function SelectionOfProducts(){
+    const products = [
+        {name:'AMD Ryzen 5 3600'},
+        {name:'Intel i5-10400F'},
+        {name:'NVIDIA RTX 3060'},
+        {name:'Corsair Vengeance 16GB RAM'},
+        {name:'Samsung 970 EVO 1TB SSD'},
+        {name:'ASUS TUF Gaming X570'}
+    ];
+}
