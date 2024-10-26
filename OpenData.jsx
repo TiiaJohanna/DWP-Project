808cfaa83dc40139300aad1c6352b0604876441e
TiiaJohanna <n3titi00@students.oamk.fi>
2024-10-26 16:45:01 +0300

    for now the opendata shows and looks good

--- OpenData.jsx
+++ OpenData.jsx
@@ -1,1 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+
+function CocktailData(){
+    const [cocktails, setCocktails] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+        .then(response => {
+            setCocktails(response.data.drinks);
+            setLoading(false);
+        });
+    }, []);
+    
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
+
+    return(
+        <div>
+            <h2>Margarita Cocktails</h2>
+            <ul>
+                {cocktails.map(drink => (
+                    <li key={drink.idDrink}>
+                        <h3>{drink.strDrink}</h3>
+                        <img src={drink.strDrinkThumb} alt={drink.strDrink} width="100" />
+                        <p>
+                            {drink.strInstructions}
+                        </p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default CocktailData;
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import CocktailData from "../OpenData"
 import Header from "./Header"
 import SelectionOfProducts from "./OrderForm"
 
@@ -7,6 +8,9 @@ function App() {
     <div>
       <Header />
       <SelectionOfProducts />
+      <br></br>
+      <br></br>
+      <CocktailData />
     </div>
   )
 }
