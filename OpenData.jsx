8a6cf5ad0a127654ec93d053da3e94377a54a253
TiiaJohanna <n3titi00@students.oamk.fi>
2024-10-26 17:33:45 +0300

    added new comments to the code how it works

--- OpenData.jsx
+++ OpenData.jsx
@@ -1,23 +1,32 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-
+// Component to fetch and display cocktail data
 function CocktailData(){
     const [cocktails, setCocktails] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
+    // Fetches data from API when the component is first rendered
     useEffect(() => {
         axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
         .then(response => {
             setCocktails(response.data.drinks);
             setLoading(false);
-        });
+        })
+        .catch(error => {
+            setError(error.message);
+            setLoading(false);
+        })
     }, []);
     
+    // Displays an error message while data is being fetched
     if (loading) return <p>Loading...</p>;
+
+    // Displays an error message if there's an error in fetching data
     if (error) return <p>Error: {error}</p>;
 
+    // Renders the list of coctails with images and instructions
     return(
         <div>
             <h2>Margarita Cocktails</h2>
